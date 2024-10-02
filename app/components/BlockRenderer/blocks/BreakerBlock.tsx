80d31fbc559eeba6c3dc9d7d33805c15dc086a93
Joe simpson <joe@egmrc.com>
2024-10-02 16:51:54 -0600

    update breaker

--- app/components/BlockRenderer/blocks/BreakerBlock.tsx
+++ app/components/BlockRenderer/blocks/BreakerBlock.tsx
@@ -28,7 +28,7 @@ const BreakerBlock = ({block}:{block:{
         blockType: 'Breaker';
     }}) => {
 
-    return <div className="w-full max-w-[calc(1800px-3.5rem)] px-7 mx-auto text-white bg-black p-8 my-24">
+    return <div className="w-full max-w-[calc(1800px-3.5rem)] px-7 mx-auto text-white bg-black p-8 my-14">
         <div className="w-full max-w-screen-2xl ml-auto flex flex-wrap gap-8 text-3xl">
             {
                 block.heading_1 ? <h2 className={`${block.heading_1.highlight ? "text-brand-yellow uppercase":""}`}>{block.heading_1.text}</h2> : ""
