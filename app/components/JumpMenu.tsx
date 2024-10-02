de4bc233dc5d90a322fc08df9834f8944bfdbcc6
Joe simpson <joe@egmrc.com>
2024-10-02 10:01:30 -0600

    Fix jump menu

--- app/components/JumpMenu.tsx
+++ app/components/JumpMenu.tsx
@@ -12,8 +12,9 @@ const JumpMenu = ({items}:{items:{id?: string|null, title?: string|null, interna
     return <div className="flex gap-8 bg-grey pl-6 pb-3 pt-4 pr-24">
         <p className="font-bold mr-4">Jump to</p>
         {items.map(item => {
+
             if (typeof item.link?.value === "number") return <Fragment key={item.id}></Fragment>
-            return <Link className="border-b-2 font-bold border-black" key={item.id} href={`/${item.link?.value?.full_path || "" + "/#"+item.internal_link}`}>{item.title}</Link>
+            return <Link className="border-b-2 font-bold border-black" key={item.id} href={`${(item.link?.value?.full_path ? "/"+item.link.value.full_path : "") + "#"+item.internal_link}`}>{item.title}</Link>
         })}
     </div>
 }
