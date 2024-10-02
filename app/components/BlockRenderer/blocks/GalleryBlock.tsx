aa8bf32aa363e22dc61a64c8d805b3b2a9937f67
Joe simpson <joe@egmrc.com>
2024-10-02 16:10:51 -0600

    Add Caption

--- app/components/BlockRenderer/blocks/GalleryBlock.tsx
+++ app/components/BlockRenderer/blocks/GalleryBlock.tsx
@@ -9,7 +9,7 @@ export const GalleryBlock = ({block}: {
     block: {
         items?:
             | {
-            image: Media;
+            image: Media|undefined;
             caption?: string | null;
             id?: string | null;
         }[]
@@ -23,15 +23,15 @@ export const GalleryBlock = ({block}: {
     return <Gallery withCaption  id={block.id||"0"}>
         <div className="flex flex-wrap gap-3">
             {block.items.map(item => {
-                return <Item caption={`<h2 class="text-4xl font-ptserif">${item.caption||""}</h2>`||undefined} key={item.id} original={item.image.url || ""}
-                             thumbnail={item.image.sizes?.thumbnail?.url || ""}
-                             height={item.image.height || 0}
-                             width={item.image.width || 0}>
+                return <Item caption={`<h2 class="text-4xl font-ptserif">${item.caption||""}</h2>`||undefined} key={item.id} original={item?.image?.url || ""}
+                             thumbnail={item?.image?.sizes?.thumbnail?.url || ""}
+                             height={item?.image?.height || 0}
+                             width={item?.image?.width || 0}>
                     {({ref, open}) => (
-                        <Image ref={ref} onClick={open} key={item.id} src={item.image.sizes?.thumbnail?.url || ""}
-                               alt={item.image.alt || ""}
-                               height={item.image.sizes?.thumbnail?.height || 0}
-                               width={item.image.sizes?.thumbnail?.width || 0}/>
+                        <Image ref={ref} onClick={open} key={item.id} src={item?.image?.sizes?.thumbnail?.url || ""}
+                               alt={item?.image?.alt || ""}
+                               height={item?.image?.sizes?.thumbnail?.height || 0}
+                               width={item?.image?.sizes?.thumbnail?.width || 0}/>
                     )}
                 </Item>
             })}
