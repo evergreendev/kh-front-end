5028207de0798eeb5fa4bc77a4454a4b3b83f4b5
Joe simpson <joe@egmrc.com>
2024-10-04 10:19:30 -0600

    fix breaker bar

--- app/components/BlockRenderer/blocks/BreakerBlock.tsx
+++ app/components/BlockRenderer/blocks/BreakerBlock.tsx
@@ -28,13 +28,13 @@ const BreakerBlock = ({block}:{block:{
         blockType: 'Breaker';
     }}) => {
 
-    return <div className="w-full max-w-[calc(1800px)] px-7 mx-auto text-white bg-black p-8 my-14">
-        <div className="w-full max-w-screen-2xl ml-auto flex flex-wrap gap-8 text-3xl">
+    return <div className="w-full max-w-[calc(1800px)] px-7 mx-auto text-white bg-black p-8 pr-24 my-14">
+        <div className="w-full max-w-screen-2xl tracking-widest ml-auto flex flex-wrap gap-4 text-2xl justify-end">
             {
-                block.heading_1 ? <h2 className={`${block.heading_1.highlight ? "text-brand-yellow uppercase":""}`}>{block.heading_1.text}</h2> : ""
+                block.heading_1 ? <h2 className={`leading-relaxed ${block.heading_1.highlight ? "text-brand-yellow uppercase":""}`}>{block.heading_1.text}</h2> : ""
             }
             {
-                block.heading_2 || block.button ? <h3 className={`max-w-[48ch] ${block.heading_2?.highlight ? "text-brand-yellow uppercase":""}`}><span className="mr-6">{block.heading_2?.text}</span>
+                block.heading_2 || block.button ? <h3 className={`max-w-[56ch] leading-relaxed ${block.heading_2?.highlight ? "text-brand-yellow":""}`}><span className="mr-6">{block.heading_2?.text}</span>
                     {block.button && (block.button.link?.title || block.button.link?.external_url || block.button.link?.Relation)
                         ? <Button isInline icon={faChevronCircleRight} config={buttonConfig[block.button.type||"primary"]} text={block.button.link?.title||""} href={block.button.link?.external ? block.button.link.external_url||"" : (block.button.link?.Relation?.value as Page)?.full_path||""}/>
                         : ""
