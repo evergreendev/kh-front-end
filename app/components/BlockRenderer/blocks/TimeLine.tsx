33927c188eff67c3b432eb8219a9d96cbd48f283
Joe simpson <joe@egmrc.com>
2024-10-07 12:47:35 -0600

    update timeline

--- app/components/BlockRenderer/blocks/TimeLine.tsx
+++ app/components/BlockRenderer/blocks/TimeLine.tsx
@@ -33,18 +33,20 @@ const TimeLine = ({block}: {
     }
 }) => {
 
-    return <div className="max-w-screen-xl mx-auto flex flex-col items-center">
+    return <div className="max-w-[1800px] mx-auto flex flex-col items-center">
         {
             block.items?.map((item, index) => {
-                return <div key={item.id} className={`mb-16 lg:mb-0 flex ${index % 2 === 0 ? "bg-pale-2" : "bg-pale-1"} w-full flex-wrap`}>
+                return <div key={item.id}
+                            className={`mb-16 lg:mb-0 flex ${index % 2 === 0 ? "bg-pale-2" : "bg-pale-1"} w-full flex-wrap lg:flex-nowrap`}>
                     <div className="flex flex-col grow p-5">
                         <p className="font-bold text-xl">{item.date}</p>
                         <h3 className="font-ptserif text-3xl mb-2">{item.title}</h3>
-                        <div className="my-auto">{renderText(item.body?.root,0,item.id||"")}</div>
+                        <div className="my-auto">{renderText(item.body?.root, 0, item.id || "")}</div>
                     </div>
                     {
                         item.image && typeof item.image !== "number" ?
-                            <Image className="w-1/3 grow h-auto" src={item.image.url || ""} alt={item.image.alt || ""} width={item.image.width || 0}
+                            <Image className="w-5/12 grow h-auto" src={item.image.url || ""} alt={item.image.alt || ""}
+                                   width={item.image.width || 0}
                                    height={item.image.height || 0}/> : ""
                     }
                 </div>
