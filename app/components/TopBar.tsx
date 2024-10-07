f20a898e6c44df8d2b175d7e31c3a6bbdcf1ddda
Joe simpson <joe@egmrc.com>
2024-10-07 17:00:22 -0600

    make top bar mobile friendly

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -8,28 +8,34 @@ import SearchBar from "@/app/components/Search";
 
 const TopBar = ({siteOption, nav}: { siteOption: SiteOption, nav: Navigation }) => {
 
-    return <div className="flex flex-wrap max-w-screen-2xl w-full justify-end lg:justify-between">
-        <Link href='/' className="lg:border-r-[3px] border-black lg:pr-8 flex lg:mr-8 mx-auto sm:mx-0 grow md:grow-0">
+    return <div className="flex flex-wrap max-w-screen-2xl w-full justify-end lg:justify-between items-center">
+        <Link href='/' className="lg:border-r-[3px] border-black lg:pr-8 flex lg:mr-8 mx-auto sm:mx-0 md:grow-0">
             {
-                typeof siteOption.siteLogoSmall !== "number" ? <Image className="max-w-md mx-auto my-3 w-full grow lg:w-96"
-                                                                      src={`${siteOption.siteLogoSmall.url}`}
-                                                                      alt={siteOption.siteLogoSmall.alt || ""}
-                                                                      width={siteOption.siteLogoSmall.width || "0"}
-                                                                      height={siteOption.siteLogoSmall.height || "0"}/>
+                typeof siteOption.siteLogoSmall !== "number" ?
+                    <Image className="max-w-md mx-auto my-3 w-full grow lg:w-96"
+                           src={`${siteOption.siteLogoSmall.url}`}
+                           alt={siteOption.siteLogoSmall.alt || ""}
+                           width={siteOption.siteLogoSmall.width || "0"}
+                           height={siteOption.siteLogoSmall.height || "0"}/>
                     : ""
             }
 
         </Link>
 
-        <div className="flex flex-col ml-auto mb-4 lg:mb-0 lg:ml-0 w-full sm:w-auto items-end lg:items-start">
+        <div className="flex-col ml-auto mb-4 lg:mb-0 lg:ml-0 w-full sm:w-auto items-end lg:items-start hidden lg:flex">
             <MegaMenu nav={nav}/>
             <div className="mt-auto hidden lg:block">
                 <SearchBar/>
             </div>
 
         </div>
+
         <div className="ml-auto">
-            <Button config={buttonConfig.highlight + " mb-5 self-start ml-auto w-64  text-center justify-around"} text="DONATE" href="https://donate.crazyhorsememorial.org/"/>
+            <Button config={buttonConfig.highlight + " mb-5 self-start ml-auto w-48  text-center justify-around"}
+                    text="DONATE" href="https://donate.crazyhorsememorial.org/"/>
+        </div>
+        <div className="flex-col ml-auto mb-4 lg:mb-0 lg:ml-0 w-full items-end lg:items-start lg:hidden flex">
+            <MegaMenu nav={nav}/>
         </div>
     </div>
 }
