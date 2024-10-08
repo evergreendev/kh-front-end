8b0352da6c59daaaa23d11ab2949e81f5189ec50
Joe simpson <joe@egmrc.com>
2024-10-08 15:11:47 -0600

    Improve mega menu

--- app/components/MegaMenu.tsx
+++ app/components/MegaMenu.tsx
@@ -1,6 +1,7 @@
 "use client"
 import {faBars} from "@awesome.me/kit-2a2dc088e2/icons/classic/solid";
-import {faChevronRight, faChevronDown, faCircleX} from "@awesome.me/kit-2a2dc088e2/icons/classic/light";
+import {faChevronRight, faChevronDown} from "@awesome.me/kit-2a2dc088e2/icons/classic/light";
+import {faCircleX} from "@awesome.me/kit-2a2dc088e2/icons/classic/regular";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {createRef, ForwardedRef, forwardRef, RefObject, useCallback, useEffect, useRef, useState} from "react";
 import {Navigation, Page} from "@/app/types/payloadTypes";
@@ -129,7 +130,6 @@ const MegaMenu = ({nav}: { nav: Navigation }) => {
              ${isExpanded ? '' : '-translate-y-full'}
              top-0 left-1/2
              -translate-x-1/2
-             flex
              fixed
              flex-wrap
              w-full
@@ -140,7 +140,7 @@ const MegaMenu = ({nav}: { nav: Navigation }) => {
              p-0
              pt-0
              duration-700 
-             z-40 transition-all`}>
+             z-40 transition-all hidden lg:flex`}>
             <div className="flex w-full bg-slate-900 shadow justify-center">
                 {
                     nav.items.map((item, index) => {
@@ -190,6 +190,90 @@ const MegaMenu = ({nav}: { nav: Navigation }) => {
                                     <h2 className="border-b-4 border-b-brand-yellow text-center justify-self-center font-ptserif text-4xl font-bold text-gray-950 mb-6">{item.title}</h2>
                                 </div>
                                 <div className="flex flex-wrap w-full p-8">
+                                    {item.columns?.map((column, index) => {
+                                        return <div key={column.id}
+                                                    className={`flex flex-col ${widths[column.width || "1/4"]}`}>
+                                            <BlockRenderer
+                                                ref={index === 0 ? firstFocusableElements.current[item.id || ""] : null}
+                                                tabIndex={item.id === activeMenuId && isExpanded ? 0 : -1}
+                                                key={column.id}
+                                                blocks={column.content}/>
+                                        </div>
+                                    })}
+                                </div>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+        <div
+            aria-hidden={!isExpanded}
+            className={`
+             ${isExpanded ? '' : '-translate-y-full'}
+             top-0 left-1/2
+             -translate-x-1/2
+             fixed
+             flex-wrap
+             w-full
+             items-start
+             overflow-hidden
+             font-opensans
+             justify-between
+             p-0
+             pt-0
+             duration-700 
+             z-40 transition-all lg:hidden flex`}>
+            <div className="flex flex-col w-full bg-slate-900 shadow justify-center">
+                {
+                    nav.items.map((item, index) => {
+                        return <div className="flex" key={item.id}>
+                            {
+                                item.columns?.length === 0 ? <>
+                                    {item.external
+                                        ?
+                                        <a className={`bg-gray-900 flex border-l border-l-slate-800 border-t-4 border-transparent group text-white hover:border-t-brand-yellow`}
+                                           href={item.external_url || ""}>
+                                    <span
+                                        className="flex items-center p-4 hover:bg-gray-900 hover:text-white group-focus:bg-gray-700">{item.title || (item.Relation?.value as Page)?.title}</span>
+                                        </a>
+                                        : <Link
+                                            className={`bg-gray-900 flex border-l border-l-slate-800 border-t-4 border-transparent group text-white hover:border-t-brand-yellow`}
+                                            href={getSlugFromCollection((item.Relation?.value as Page), item.Relation?.relationTo || "page")}>
+                                    <span
+                                        className="flex items-center p-4 hover:bg-gray-900 hover:text-white group-focus:bg-gray-700">{item.title || (item.Relation?.value as Page)?.title}</span>
+                                        </Link>}
+                                </> : <>
+                                    <ExpandableButton isExpanded={activeMenuId === item.id}
+                                                      firstFocusable={firstFocusableElements.current[item.id || ""]}
+                                                      ref={index === 0 ? firstFocusableElementRef : null}
+                                                      tabIndex={isExpanded ? 0 : -1} item={item}
+                                                      setActiveMenuId={() => {
+                                                          setActiveMenuId(item.id || null)
+                                                      }} id={item.id || ""}
+                                                      text={item.title || (item.Relation?.value as Page)?.title}/>
+                                </>
+                            }
+                        </div>
+                    })
+                }
+                <button onClick={() => {
+                    setActiveMenuId(null)
+                    setIsExpanded(false)
+                }} className="text-white z-50 right-0 top-0 p-2 bg-slate-700 hover:bg-slate-800">Close Menu
+                </button>
+            </div>
+            <div className="w-full mx-auto max-w-[2000px] h-screen overflow-hidden p-0">
+                {
+                    nav.items.map(item => {
+                        return <div key={item.id}
+                                    className={`bg-gray-200 absolute z-50 w-full overflow-hidden top-0 left-0 bottom-0 right-0 transition-all ${item.id === activeMenuId ? "z-10" : "-translate-x-full"}`}>
+                            <button className="absolute p-4 right-0 text-slate-800" onClick={()=> setActiveMenuId(null)}><FontAwesomeIcon size="2x" icon={faCircleX}/></button>
+                            <div className="p-0 flex flex-wrap flex-col">
+                                <div className="flex flex-col w-full justify-around bg-gray-300 pt-4">
+                                    <h2 className="border-b-4 border-b-brand-yellow text-center justify-self-center font-ptserif text-4xl font-bold text-gray-950 mb-6">{item.title}</h2>
+                                </div>
+                                <div className="flex flex-col flex-wrap w-full p-8">
                                     {item.columns?.map((column, index) => {
                                         return <div key={column.id}
                                                     className={`flex flex-col ${widths[column.width || "1/4"]}`}>
