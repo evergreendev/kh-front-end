6f5a149aab08789a19c06763a164cb632155ef41
Joe simpson <joe@egmrc.com>
2024-10-16 09:41:40 -0600

    Add book tickets widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {open_sans, pt_serif} from "@/app/fonts";
 import "./globals.css";
+import Script from "next/script";
 
 async function getMeta() {
     const res = await fetch(
@@ -14,8 +15,7 @@ async function getMeta() {
 }
 
 
-export async function generateMetadata(
-): Promise<Metadata> {
+export async function generateMetadata(): Promise<Metadata> {
 
     const meta = await getMeta();
 
@@ -27,13 +27,16 @@ export async function generateMetadata(
 }
 
 export default async function RootLayout({
-                                       children,
-                                   }: Readonly<{
+                                             children,
+                                         }: Readonly<{
     children: React.ReactNode;
 }>) {
     const meta = await getMeta();
     return (
         <html lang="en">
+        <Script
+            src="//https://linkprotect.cudasvc.com/url?a=https%3a%2f%2f%2f%2ftag.brandcdn.com%2fautoscript%2fcrazyhorsememorial_vgtsqk5fmvvsve09%2fCrazy_Horse_Memorial.js&c=E,1,-HJw6C0kycDXGRiVnsdST6VP3vcPRWtgjwIUtPnXdd_37gl5XrGUyEuIsC0nt3o6YkjXuXZQ6XtHNdMpX6ul8EIUeAh3G4RAJx1QebC9fsRu4wM,&typo=1"/>
+        <Script src="https://fareharbor.com/embeds/api/v1/?autolightframe=yes"/>
         <body className={`font-opensans ${open_sans.variable} ${pt_serif.variable}`}>{children}</body>
         </html>
     );
