61885af4ab415ad7055a00848764c41723a5a352
Joe simpson <joe@egmrc.com>
2024-10-17 10:59:31 -0600

    Maybe time fix

--- app/utilities/hours.ts
+++ app/utilities/hours.ts
@@ -62,5 +62,5 @@ export const getHoursFromSchedule = (hours: Hours) => {
 
     const end = new Date(hours.hour_end);
 
-    return getHourText(start.getHours() - (start.getTimezoneOffset()/60), (start.getMinutes() < 10 ? '0' : '') + start.getMinutes()) + "-" + getHourText(end.getHours()-(end.getTimezoneOffset()/60), (end.getMinutes() < 10 ? '0' : '') + end.getMinutes());
+    return getHourText(start.getHours() + (start.getTimezoneOffset()/60), (start.getMinutes() < 10 ? '0' : '') + start.getMinutes()) + "-" + getHourText(end.getHours()+(end.getTimezoneOffset()/60), (end.getMinutes() < 10 ? '0' : '') + end.getMinutes());
 }
