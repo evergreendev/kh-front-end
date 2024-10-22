2504f4a37a00633dcceaaf62e819506ffd77a9e8
Joe simpson <joe@egmrc.com>
2024-10-22 15:38:11 -0600

    Fix Gallery block

--- app/components/BlockRenderer/blocks/GalleryBlock.tsx
+++ app/components/BlockRenderer/blocks/GalleryBlock.tsx
@@ -28,10 +28,10 @@ export const GalleryBlock = ({block}: {
                              height={item?.image?.height || 0}
                              width={item?.image?.width || 0}>
                     {({ref, open}) => (
-                        <Image className="grow" ref={ref} onClick={open} key={item.id} src={item?.image?.sizes?.thumbnail?.url || ""}
+                        <div className="grow flex" onClick={open}><Image className="grow" ref={ref} key={item.id} src={item?.image?.sizes?.thumbnail?.url || ""}
                                alt={item?.image?.alt || ""}
                                height={item?.image?.sizes?.thumbnail?.height || 0}
-                               width={item?.image?.sizes?.thumbnail?.width || 0}/>
+                                    width={item?.image?.sizes?.thumbnail?.width || 0}/></div>
                     )}
                 </Item>
             })}
